5ac6b6574daa89a03b850abdfa7da187d515563e
Yosvel Quintero <yosvelquintero@gmail.com>
2024-09-09 21:38:31 +0700

    chore: Refactor test cases for various time scenarios

--- index.test.js
+++ index.test.js
@@ -26,53 +26,58 @@ describe('Time to words', () => {
     expect(timeInWords).toBe("ten o'clock");
   });
 
-  it('Handles 30 - 8:30', () => {
+  it('Handles 8:30', () => {
     const timeInWords = convertTimeToWords('8:30');
     expect(timeInWords).toBe('half past eight');
   });
 
-  it('Handles 3 - 2:03', () => {
+  it('Handles 2:03', () => {
     const timeInWords = convertTimeToWords('2:03');
     expect(timeInWords).toBe('three past two');
   });
 
-  it('Handles 11 - 2:11', () => {
+  it('Handles 2:11', () => {
     const timeInWords = convertTimeToWords('2:11');
     expect(timeInWords).toBe('eleven past two');
   });
 
-  it('Handles 15 - 2:15', () => {
+  it('Handles 2:15', () => {
     const timeInWords = convertTimeToWords('2:15');
     expect(timeInWords).toBe('quarter past two');
   });
 
-  it('Handles 30 - 2:30', () => {
+  it('Handles 2:30', () => {
     const timeInWords = convertTimeToWords('2:30');
     expect(timeInWords).toBe('half past two');
   });
 
-  it('Handles 27 - 2:33', () => {
+  it('Handles 2:33', () => {
     const timeInWords = convertTimeToWords('2:33');
     expect(timeInWords).toBe('twenty seven to three');
   });
 
-  it('Handles 20 - 2:40', () => {
+  it('Handles 2:40', () => {
     const timeInWords = convertTimeToWords('2:40');
     expect(timeInWords).toBe('twenty to three');
   });
 
-  it('Handles 45 - 2:45', () => {
+  it('Handles 2:45', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
 
-  it('Handles 55 - 2:55', () => {
+  it('Handles 2:55', () => {
     const timeInWords = convertTimeToWords('2:55');
     expect(timeInWords).toBe('five to three');
   });
 
-  it('Handles 12 - 2:59', () => {
+  it('Handles 2:59', () => {
     const timeInWords = convertTimeToWords('2:59');
     expect(timeInWords).toBe('one to three');
+  });
+
+  it('Handles 23:59', () => {
+    const timeInWords = convertTimeToWords('23:59');
+    expect(timeInWords).toBe('one to midnight');
   });
 });
